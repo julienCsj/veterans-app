6587b54c8347c5dfee39d47c4ccf488544d64c0b
Julien Custoja <juliencustoja@MacBook-Air-de-Julien.local>
2016-10-22 10:41:06 +0200

    Amélioration chatbox

--- app/controllers/frontoffice/Chatbox.java
+++ app/controllers/frontoffice/Chatbox.java
@@ -2,9 +2,17 @@ package controllers.frontoffice;
 
 import models.ChatBoxMessage;
 import models.Compte;
+import play.data.binding.As;
 import play.mvc.Controller;
+import play.templates.BaseTemplate;
+import play.templates.JavaExtensions;
+import play.templates.Template;
+import play.templates.TemplateLoader;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by juliencustoja on 21/10/2016.
@@ -13,7 +21,9 @@ public class Chatbox extends Controller {
 
     public static void chatbox(String hash) {
         Compte compte = Compte.find("hash = ?", hash).first();
-        render(compte);
+        List<ChatBoxMessage> messagesList = ChatBoxMessage.find("silence = false").fetch(50);
+        Date dernierCheck = new Date();
+        render(compte, dernierCheck, messagesList);
     }
 
     public static void chatboxPost(String hash) {
@@ -24,9 +34,21 @@ public class Chatbox extends Controller {
         renderJSON("success");
     }
 
-    public static void messagesList(String hash) {
-        List<ChatBoxMessage> messagesList = ChatBoxMessage.find("silence = false").fetch(50);
+    public static void messagesList(String hash, @As("dd-MM-yyyy-HH:mm:ss.SSS") Date dernierCheck) {
+        System.out.println(new Date() + " -> APPEL");
+        List<ChatBoxMessage> messagesList = ChatBoxMessage.find("silence = false AND dateCreation > ?", dernierCheck).fetch(50);
         Compte compte = Compte.find("hash = ?", hash).first();
-        render(messagesList, compte);
+        Template t = TemplateLoader.load("frontoffice/Chatbox/messagesList.html");
+        Map<String, Object> args = new HashMap<>();
+        args.put("messagesList", messagesList);
+        args.put("compte", compte);
+        String html = t.render(args);
+
+        Date date = new Date();
+        String dateAsStr = JavaExtensions.format(date, "dd-MM-yyyy-HH:mm:ss.SSS", "fr");
+        Map<String, String> json = new HashMap<>();
+        json.put("html", html);
+        json.put("dernierCheck", dateAsStr);
+        renderJSON(json);
     }
 }
